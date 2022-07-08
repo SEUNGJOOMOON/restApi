d063ab7d13667f93a1873a0ff03805bc13b43797
SEUNGJOOMOON <nick901106@gmail.com>
2022-07-08 11:01:02 +0900

    add:사용자 등록 조회 삭제 추가

---
+++ src/main/java/com/study/api/helloword/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package com.study.api.helloword;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+
+}
